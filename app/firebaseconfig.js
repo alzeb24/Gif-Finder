2c6bc17db027617ebb6b400830cefdf9c444cd23
alzeb24 <alzebkhan1@gmail.com>
2023-12-21 10:44:43 +0530

    Setup Firebase project for user authentication

---
+++ app/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8cVJtjIsYFBsAE62gvwmBLuej12lpbi8",
+    authDomain: "gif-finder-a8b61.firebaseapp.com",
+    projectId: "gif-finder-a8b61",
+    storageBucket: "gif-finder-a8b61.appspot.com",
+    messagingSenderId: "928826909894",
+    appId: "1:928826909894:web:ef57b0bc48dadd3dc7a3c7",
+    measurementId: "G-LFN7WKEC77"
+};
+
+export default firebaseConfig;
